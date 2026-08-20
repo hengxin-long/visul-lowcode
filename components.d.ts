8ed3f38d39a9f3b51bc59d672834bc9623ab0d80
hengxin175 <2168374541@qq.com>
2026-08-20 14:06:20 +0800

    feat：菜单栏和菜单跳转路由

--- components.d.ts
+++ components.d.ts
@@ -12,6 +12,12 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     ElButton: typeof import('element-plus/es')['ElButton']
+    ElCol: typeof import('element-plus/es')['ElCol']
+    ElIcon: typeof import('element-plus/es')['ElIcon']
+    ElMenu: typeof import('element-plus/es')['ElMenu']
+    ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
+    ElMenuItemGroup: typeof import('element-plus/es')['ElMenuItemGroup']
+    ElSubMenu: typeof import('element-plus/es')['ElSubMenu']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
--- src/layouts/index.vue
+++ src/layouts/index.vue
@@ -1,6 +1,31 @@
 <template>
-  我是布局
-  <RouterView />
+  <div class="layout">
+    <div class="menu">
+      <el-col :span="24">
+        <h5 class="mb-2">Visul LowCode</h5>
+        <el-menu default-active="1">
+          <el-menu-item index="1" @click="$router.push({path: '/form-list'})">
+            <el-icon><icon-menu /></el-icon>
+            <span>全部表单</span>
+          </el-menu-item>
+          <el-menu-item index="2" @click="$router.push({path: '/form-design'})">
+            <el-icon>
+              <document />
+            </el-icon>
+            <span>新建表单</span>
+          </el-menu-item>
+        </el-menu>
+      </el-col>
+    </div>
+    <div class="content">
+      <div class="header">我是顶部</div>
+      <div class="main">
+        我是主体
+        <RouterView />
+      </div>
+    </div>
+  </div>
+
 </template>
 
 <script setup lang="ts">
@@ -8,4 +33,14 @@ import { RouterView } from 'vue-router';
 </script>
 
 <style scoped lang="scss">
+.layout {
+  display: flex;
+
+  .menu {
+    width: 200px;
+    height: 600px;
+    border: 1px solid gray;
+    background-color: #fff;
+  }
+}
 </style>
