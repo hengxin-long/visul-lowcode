25ba95fbf9028f86b073f7314ee2b0fcc205160f
hengxin175 <2168374541@qq.com>
2026-08-20 13:44:13 +0800

    style：修改名字

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Layout from '@/layouts/Layout.vue'
+import Layout from '@/layouts/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
