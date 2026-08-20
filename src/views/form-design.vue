ea62eb3529c1efd028322d2719ba973fed24be36
hengxin175 <2168374541@qq.com>
2026-08-20 13:33:17 +0800

    feat：添加组件

---
+++ src/views/form-design.vue
@@ -0,0 +1,9 @@
+<template>
+  我是表单设计器
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped lang="scss">
+</style>
---
+++ src/views/form-list.vue
@@ -0,0 +1,9 @@
+<template>
+  我是表单列表
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped lang="scss">
+</style>
