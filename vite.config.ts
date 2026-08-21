da40fe33b27cc55d0355a8b8ec5292177ad51a6e
hengxin175 <2168374541@qq.com>
2026-08-21 18:21:40 +0800

    chore：配置mock

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,8 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 import AutoImport from 'unplugin-auto-import/vite'
 import Component from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-import {viteMockServe} from 'vite-plugin-mock'
+// import {viteMockServe} from 'vite-plugin-mock'
+import { mockDevServerPlugin } from 'vite-plugin-mock-dev-server'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -19,9 +20,15 @@ export default defineConfig({
     Component({
       resolvers: [ElementPlusResolver({ importStyle: "sass" })]
     }),
-    viteMockServe({
-      localEnabled: true, // 开发环境开启mock
-      prodEnabled: false, // 生产关闭mock
+    // viteMockServe({
+    //   localEnabled: true, // 开发环境开启mock
+    //   prodEnabled: false, // 生产关闭mock
+    // }),
+    mockDevServerPlugin({
+      dir: 'mock', // mock 文件夹根目录
+      log: 'info', // 控制台打印mock请求日志类型
+      prefix: ['/mock'], // 只拦截匹配/mock开头的请求
+      reload: false // 热更新时刷新页面
     })
   ],
   resolve: {
