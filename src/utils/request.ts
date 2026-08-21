dac5db7fa8865b5f0a129b72a53492e6344aee37
hengxin175 <2168374541@qq.com>
2026-08-21 18:23:34 +0800

    feat：封装请求工具

---
+++ src/utils/request.ts
@@ -0,0 +1,19 @@
+
+import axios from 'axios'
+
+const request = axios.create({
+  baseURL: '',
+  timeout: 5000
+})
+
+request.interceptors.request.use(res => {
+  console.log('请求拦截config ', res)
+  return res
+})
+
+request.interceptors.response.use(res => {
+  console.log('响应拦截config ', res)
+  return res.data
+})
+
+export default request
