b900b527db410051f5cf5c1e07fba1910dd60b7d
hengxin175 <2168374541@qq.com>
2026-08-21 18:23:01 +0800

    feat：封装api

---
+++ src/api/form.ts
@@ -0,0 +1,11 @@
+import request from '@/utils/request'
+
+const BASE_FORM_USL = '/mock'
+
+export function getFormList(params: { page: number; pageSize: number }) {
+  return request({
+    url: `${BASE_FORM_USL}/form/list`,
+    method: 'GET',
+    params
+  })
+}
