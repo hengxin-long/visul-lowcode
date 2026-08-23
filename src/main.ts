7ab86a6f3a5c3890a12931aa3387594c6ea46edc
hengxin175 <2168374541@qq.com>
2026-08-23 10:53:50 +0800

    fix：导入中文包并修复UI bug

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,16 @@ import router from './router'
 import './styles/common.scss'
 // 只引入el-message组件样式，避免和定制主题色冲突
 import 'element-plus/theme-chalk/el-message.css'
+import ElementPlus from 'element-plus'
+// 中文语言包
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
 const app = createApp(App)
 
+// 将组件语言注册为中文
+app.use(ElementPlus, {
+  locale: zhCn
+})
 app.use(createPinia())
 app.use(router)
 
