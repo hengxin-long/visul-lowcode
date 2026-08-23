3246ceaa3d9bd7e94c2c5dde7059ee47cbc40be1
hengxin175 <2168374541@qq.com>
2026-08-23 09:21:58 +0800

    feat：添加删除接口

--- mock/form.mock.ts
+++ mock/form.mock.ts
@@ -6,6 +6,7 @@ export default defineMock([
   {
     url: '/mock/form/list',
     method: 'GET',
+    // query：查询参数，是 /form/list?page=1&pageSize=10 问号后面的参数
     body(res) {
       const currentPage = Number(res.query.page)
       const pageSize = Number(res.query.pageSize)
@@ -45,6 +46,32 @@ export default defineMock([
         msg
       }
     }
+  },
+  {
+    url: '/mock/form/delete/:id',
+    method: 'DELETE',
+    // params：路径参数，是 /delete/:id 路径占位符得来的
+    body({params}) {
+      console.log('id: ', params.id, typeof params.id)
+
+      let code = httpStatus.success.code
+      let msg = httpStatus.success.msg
+      const idIndex = formData.findIndex(item => item.id === params.id)
+      if (idIndex === -1) {
+        code = httpStatus.failed.code
+        msg = '删除失败'
+      }
+      formData.splice(idIndex, 1)
+      return {
+        code,
+        // data: {
+        //   data: formData,
+        //   total: formData.length
+        // },
+        data: {},
+        msg
+      }
+    }
   }
 ])
 
@@ -64,11 +91,11 @@ const formData = [
   },
   {
     id: '2',
-    formName: '报名登记表',
+    formName: '登记表',
     formType: '业务表单',
     status: 'draft',
-    createTime: '2026-08-03 09:10:00',
-    updateTime: '2026-08-03 09:10:00',
+    createTime: '2026-05-03 09:10:00',
+    updateTime: '2026-06-03 09:10:00',
     schema: {
       formName: '报名登记表',
       formType: '业务表单',
@@ -77,7 +104,7 @@ const formData = [
   },
   {
     id: '3',
-    formName: '报名登记表',
+    formName: '申请表',
     formType: '业务表单',
     status: 'draft',
     createTime: '2026-08-03 09:10:00',
@@ -90,11 +117,11 @@ const formData = [
   },
   {
     id: '4',
-    formName: '报名登记表',
+    formName: '登录表单',
     formType: '业务表单',
     status: 'draft',
     createTime: '2026-08-03 09:10:00',
-    updateTime: '2026-08-03 09:10:00',
+    updateTime: '2026-08-01 08:10:00',
     schema: {
       formName: '报名登记表',
       formType: '业务表单',
@@ -116,11 +143,11 @@ const formData = [
   },
   {
     id: '6',
-    formName: '报名登记表',
+    formName: '登录表单',
     formType: '业务表单',
     status: 'draft',
     createTime: '2026-08-03 09:10:00',
-    updateTime: '2026-08-03 09:10:00',
+    updateTime: '2026-08-01 08:11:00',
     schema: {
       formName: '报名登记表',
       formType: '业务表单',
@@ -129,11 +156,11 @@ const formData = [
   },
   {
     id: '7',
-    formName: '报名登记表',
-    formType: '业务表单',
+    formName: '问卷表单',
+    formType: '调查表单',
     status: 'draft',
     createTime: '2026-08-03 09:10:00',
-    updateTime: '2026-08-03 09:10:00',
+    updateTime: '2026-06-02 12:10:00',
     schema: {
       formName: '报名登记表',
       formType: '业务表单',
@@ -207,11 +234,11 @@ const formData = [
   },
   {
     id: '13',
-    formName: '报名登记表',
+    formName: '报名表',
     formType: '业务表单',
     status: 'draft',
-    createTime: '2026-08-03 09:10:00',
-    updateTime: '2026-08-03 09:10:00',
+    createTime: '2026-03-03 09:10:00',
+    updateTime: '2026-07-03 09:10:00',
     schema: {
       formName: '报名登记表',
       formType: '业务表单',
@@ -220,10 +247,10 @@ const formData = [
   },
   {
     id: '14',
-    formName: '报名登记表',
+    formName: '登记表',
     formType: '业务表单',
     status: 'draft',
-    createTime: '2026-08-03 09:10:00',
+    createTime: '2026-04-03 09:10:00',
     updateTime: '2026-08-03 09:10:00',
     schema: {
       formName: '报名登记表',
@@ -233,7 +260,7 @@ const formData = [
   },
   {
     id: '15',
-    formName: '报名登记表',
+    formName: '登录表单',
     formType: '业务表单',
     status: 'draft',
     createTime: '2026-08-03 09:10:00',
