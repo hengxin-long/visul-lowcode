b91e2a27949f99fd8b199d8144d80c0990f4ea6f
hengxin175 <2168374541@qq.com>
2026-08-26 20:06:20 +0800

    style：添加样式

--- src/views/form-list/CountCard.vue
+++ src/views/form-list/CountCard.vue
@@ -6,8 +6,8 @@
           <p class="title">{{ item.title }}</p>
           <p class="count">{{ item.count }}</p>
         </div>
-        <div class="iconfont">
-          <el-icon :size="22">
+        <div class="iconfont" :style="`background-color: ${item.iconBackgroundColor}`">
+          <el-icon :size="22" :style="`color: ${item.iconColor}`">
             <MessageBox />
           </el-icon>
         </div>
@@ -21,29 +21,49 @@ import { MessageBox } from '@element-plus/icons-vue'
 import { onMounted, ref } from 'vue'
 import { useFormStore } from '@/stores/form'
 import { storeToRefs } from 'pinia';
+import type { Card } from '@/types/card';
+import type {Ref} from 'vue'
+
 
 defineOptions({ name: 'CountCard' })
 
 const formStore = useFormStore()
 const { all, published } = storeToRefs(formStore)
+console.log('all', all)
 const { countForm } = formStore
 
-const cardData = ref([
+const cardData = ref<Card<Ref>[]>([
   {
     title: '全部',
     count: all,
+    icon: '',
+    backgroundColor: '',
+    iconColor: '#4BED4BFF',
+    iconBackgroundColor: '#B5FFB5FF'
   },
   {
     title: '已发布',
     count: published,
+    icon: '',
+    backgroundColor: '',
+    iconColor: '#5AD2F0FF',
+    iconBackgroundColor: '#BDF0FDFF'
   },
   {
     title: '本月收集',
-    count: 3478,
+    count: ref(3785),
+    icon: '',
+    backgroundColor: '',
+    iconColor: '#FAAF47FF',
+    iconBackgroundColor: '#FFD59BFF'
   },
   {
     title: '待处理数据',
-    count: 186,
+    count: ref(186),
+    icon: '',
+    backgroundColor: '',
+    iconColor: '#DB5FFDFF',
+    iconBackgroundColor: '#F1C0FFFF'
   },
 ])
 
@@ -77,6 +97,15 @@ onMounted(() => {
         margin-top: 15px;
         font-size: 27px;
         font-weight: 600;
+      }
+
+      .iconfont {
+        width: 40px;
+        height: 40px;
+        border-radius: 10px;
+        // background-color: rgb(133, 238, 133);
+        text-align: center;
+        line-height: 50px;
       }
     }
   }
