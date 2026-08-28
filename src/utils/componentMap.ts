5b8f9a5735f1572f980b56e7afc7f6928f8aa99f
hengxin175 <2168374541@qq.com>
2026-08-28 09:50:50 +0800

    feat：组件映射表

---
+++ src/utils/componentMap.ts
@@ -0,0 +1,18 @@
+
+import { 
+  ElInput,
+  ElInputNumber,
+  ElCheckbox,
+  ElRadio,
+  ElDatePicker
+ } from "element-plus/lib/components";
+
+//  组件映射表
+export const componentMap = {
+  input: ElInput,
+  password: ElInput,
+  inputNumber: ElInputNumber,
+  checkbox: ElCheckbox,
+  radio: ElRadio,
+  Date: ElDatePicker
+}
