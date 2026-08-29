eb21fb41978c80dc0ee1ad45e04d30a73360e031
hengxin175 <2168374541@qq.com>
2026-08-29 19:12:07 +0800

    fix：修复scrollbar显示bug

--- src/views/form-design/DesignComponentLibrary.vue
+++ src/views/form-design/DesignComponentLibrary.vue
@@ -8,17 +8,60 @@
       <div class="components">
         <el-collapse v-model="activeName" accordion>
           <el-collapse-item title="基础字段" name="1">
-            <!-- <el-scrollbar max-height="200px"> -->
-            <div v-for="field in fields" class="field" :key="field.label" :data-field="JSON.stringify(field)">
-              {{ field.label }}
-            </div>
-            <!-- </el-scrollbar> -->
+            <el-scrollbar max-height="220px">
+              <div class="fields-box">
+                <div v-for="field in fields" class="field" :key="field.label" :data-field="JSON.stringify(field)">
+                  <p>
+                    <el-icon class="icon">
+                      <SetUp />
+                    </el-icon>
+                  </p>
+                  <p>{{ field.label }}</p>
+                </div>
+              </div>
+            </el-scrollbar>
           </el-collapse-item>
           <el-collapse-item title="主页字段" name="2">
-            <div>字段</div>
+            <el-scrollbar max-height="220px">
+              <div class="fields-box">
+                <div v-for="field in fields" class="field" :key="field.label" :data-field="JSON.stringify(field)">
+                  <p>
+                    <el-icon class="icon">
+                      <SetUp />
+                    </el-icon>
+                  </p>
+                  <p>{{ field.label }}</p>
+                </div>
+              </div>
+            </el-scrollbar>
           </el-collapse-item>
           <el-collapse-item title="表单字段" name="3">
-            <div>字段</div>
+            <el-scrollbar max-height="220px">
+              <div class="fields-box">
+                <div v-for="field in fields" class="field" :key="field.label" :data-field="JSON.stringify(field)">
+                  <p>
+                    <el-icon class="icon">
+                      <SetUp />
+                    </el-icon>
+                  </p>
+                  <p>{{ field.label }}</p>
+                </div>
+              </div>
+            </el-scrollbar>
+          </el-collapse-item>
+          <el-collapse-item title="图表字段" name="4">
+            <el-scrollbar max-height="220px">
+              <div class="fields-box">
+                <div v-for="field in fields" class="field" :key="field.label" :data-field="JSON.stringify(field)">
+                  <p>
+                    <el-icon class="icon">
+                      <SetUp />
+                    </el-icon>
+                  </p>
+                  <p>{{ field.label }}</p>
+                </div>
+              </div>
+            </el-scrollbar>
           </el-collapse-item>
         </el-collapse>
       </div>
@@ -34,13 +77,14 @@ import { onMounted, onUnmounted, ref } from 'vue';
 import Sortable from 'sortablejs'
 import { baseFields } from '@/utils/materialList';
 import type { FormComponent } from '@/types/form'
+import { SetUp } from '@element-plus/icons-vue'
 
 const activeName = ref('1')
 const fields = ref<FormComponent[]>(baseFields)
 let sortbale: Sortable
 
 onMounted(() => {
-  const form = document.querySelector('.el-collapse-item__content') as HTMLElement
+  const form = document.querySelector('.fields-box') as HTMLElement
   sortbale = new Sortable(form, {
     group: {
       name: 'shared', // 组名
@@ -82,25 +126,44 @@ onUnmounted(() => {
 
   .components {
     width: 100%;
-    height: 400px;
+    // height: 400px;
     margin: 20px 0;
 
-    :deep(.el-collapse-item__content) {
-      display: flex;
-      flex-wrap: wrap;
-      gap: 10px;
-    }
-
     .field {
-      width: 80px;
-      height: 50px;
+      width: 100%;
+      height: 45px;
+      padding: 2px 8px;
       background-color: #fff;
       border: 1px solid var(--border-color);
-      border-radius: 10px;
-      font-size: 14px;
+      border-radius: 8px;
       text-align: center;
-      line-height: 50px;
+      // line-height: 40px;
       cursor: pointer;
+
+      p {
+        font-size: 12px;
+        height: 20px;
+        line-height: 20px;
+        text-align: start;
+      }
+
+      .icon {
+        display: inline;
+        color: $vlcpColor;
+      }
+
+    }
+
+    :deep(.el-scrollbar) {
+      width: 100%;
+
+    }
+
+    .fields-box {
+      display: grid;
+      grid-template-columns: repeat(2, auto) !important;
+      gap: 10px;
+      width: 100%;
     }
   }
 
